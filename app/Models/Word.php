3e00357282517a562188ec276608fd01d4a3c2cf
KasraMehr <kasramehrali@gmail.com>
2025-03-17 05:34:38 -0400

    added imports to use model

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
+use App\Jobs\IndexWordInElasticsearch;
+
 
 /**
  * @method static findOrFail($id)
