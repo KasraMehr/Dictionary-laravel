1f3cfa036b51a9f423b0aa8b243bf77760fe8b1c
KasraMehr <kasramehrali@gmail.com>
2025-03-17 05:35:36 -0400

    added categories

--- resources/js/Pages/Words/Word.vue
+++ resources/js/Pages/Words/Word.vue
@@ -25,6 +25,13 @@ defineProps({
                 <main class="mt-6 flex justify-center ">
                     <div class="lg:w-2/3 w-full flex flex-col gap-2 items-start overflow-hidden rounded-lg bg-gradient-to-br from-gray-400/50 to-gray-200/50 dark:from-gray-800/50 dark:to-gray-700/50 p-6 shadow-lg ring-1 ring-white/10 transition duration-300 hover:ring-white/20 hover:shadow-xl hover:shadow-[#FF2D20]/10 lg:p-10 lg:pb-10 backdrop-blur-sm">
                       <div class="text-3xl font-bold text-gray-900 dark:text-white">{{ word.word }}</div>
+                      <div class="flex justify-start gap-3 xl:gap-2 w-full ml-auto">
+                          <span v-for="category in (word.categories ? word.categories.slice(0, 3) : [])"
+                              :key="category.id"
+                              class="bg-gray-400 dark:bg-gray-600 dark:text-white text-black text-xs px-3 py-1 rounded-xl">
+                              {{ category . name }}
+                          </span>
+                      </div>
                       <div class="text-lg font-semibold text-gray-700 dark:text-gray-300">{{ word.meaning }}</div>
                       <div class="text-base text-gray-600 dark:text-gray-400 italic">{{ word.pronunciation }}</div>
                       <div class="text-sm text-gray-500 dark:text-gray-400 leading-relaxed">{{ word.description }}</div>
