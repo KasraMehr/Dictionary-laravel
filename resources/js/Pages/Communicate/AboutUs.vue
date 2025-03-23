3a0024025bc8c992b0f1c5e6cffabf44150677cd
KasraMehr <kasramehrali@gmail.com>
2025-03-23 08:41:35 -0400

    fixed subscribe colors and style

--- resources/js/Pages/Communicate/AboutUs.vue
+++ resources/js/Pages/Communicate/AboutUs.vue
@@ -607,16 +607,16 @@ onMounted(() => {
 
     <section class="py-20 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div class="bg-red-600 rounded-2xl p-8 xl:p-11">
+            <div class="bg-gray-600 rounded-2xl p-8 xl:p-11">
                 <h2 class="font-manrope text-4xl text-white text-center font-bold mb-4">Subscribe to the latest offer
                 </h2>
-                <p class="text-red-200 text-center mb-11 max-lg:max-w-2xl mx-auto">Join our community of subscribers
+                <p class="text-gray-200 text-center mb-11 max-lg:max-w-2xl mx-auto">Join our community of subscribers
                     and receive regular
                     updates delivered straight to your inbox. It's quick, easy, and free</p>
                 <div
-                    class="max-w-md mx-auto lg:bg-transparent lg:border border-gray-300 rounded-3xl max-lg:py-3 lg:rounded-full lg:h-12 lg:p-1.5 lg:flex-row gap-6 lg:gap-0 flex-col flex items-center justify-between">
+                    class="max-w-md mx-auto lg:bg-transparent rounded-3xl max-lg:py-3 lg:rounded-full lg:h-12 lg:p-1.5 lg:flex-row gap-6 lg:gap-0 flex-col flex items-center justify-between">
                     <input type="text" name="email"
-                        class="py-2 px-6 bg-transparent rounded-full max-lg:border border-gray-300  text-gray-100 max-lg:text-center placeholder:text-gray-400 focus:outline-none flex-1 w-full lg:w-auto lg:py-2 lg:px-6 lg:bg-transparent"
+                        class="py-2 px-6 bg-transparent rounded-full text-gray-100 max-lg:text-center placeholder:text-gray-400 focus:outline-none flex-1 w-full lg:w-auto lg:py-2 lg:px-6 lg:bg-transparent mx-2"
                         placeholder="Enter your email..">
                     <button type="submit"
                         class="py-2 px-5 text-sm bg-red-500 shadow-md rounded-full  text-white font-semibold hover:bg-red-700">Subscribe</button>
