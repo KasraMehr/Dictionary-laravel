7adfe4349f0ce5ad760262674784697e3820187b
KasraMehr <kasramehrali@gmail.com>
2025-03-24 16:31:31 -0400

    final version of faq

--- resources/js/Pages/Communicate/FAQ.vue
+++ resources/js/Pages/Communicate/FAQ.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import MainLayout from "@/Layouts/MainLayout.vue";
 
@@ -26,6 +26,11 @@ const activeIndex = ref(null);
 const toggleAccordion = (index) => {
   activeIndex.value = activeIndex.value === index ? null : index;
 };
+
+// نظارت بر تغییر زبان و ریلود صفحه
+watch(locale, () => {
+  location.reload();
+});
 </script>
 
 <template>
@@ -36,56 +41,65 @@ const toggleAccordion = (index) => {
             FAQs
         </h6>
         <h1 class="text-3xl font-manrope font-bold text-gray-900 dark:text-gray-100 text-center mb-8">
-            Frequently Asked Questions
+            {{ $t('more_faq') }}
         </h1>
-
-
-        <div
-          v-for="(faq, index) in faqs"
-          :key="index"
-          class="border border-gray-700 dark:border-gray-200 p-4 rounded-xl mb-4 transition-all duration-300 overflow-hidden"
-          :class="{ 'bg-red-50 border-red-600 shadow-md': activeIndex === index }"
-        >
-          <!-- دکمه سوال -->
-          <button
-            @click="toggleAccordion(index)"
-            class="group flex items-center justify-between text-left text-lg font-normal leading-8 text-gray-900 dark:text-gray-100 w-full transition duration-300 hover:text-red-600"
-          >
-            <h5 :class="{ 'font-medium text-red-600': activeIndex === index }">
-              {{ faq.question }}
-            </h5>
-
-            <!-- آیکون تغییر -->
-            <svg
-              class="w-6 h-6 transition-transform duration-300"
-              :class="{ 'rotate-180 text-red-600': activeIndex === index }"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6 12H18M12 18V6"
-                stroke="currentColor"
-                stroke-width="1.6"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
-          </button>
-
-          <!-- جواب با انیمیشن باز و بسته شدن -->
-          <div
-            class="transition-all duration-500 ease-in-out"
-            :style="{
-              maxHeight: activeIndex === index ? '150px' : '0px',
-              opacity: activeIndex === index ? 1 : 0,
-              transform: activeIndex === index ? 'translateY(0)' : 'translateY(-10px)'
-            }"
-          >
-            <p class="text-base text-gray-900 dark:text-gray-100 font-normal leading-6 p-3">
-              {{ faq.answer }}
-            </p>
+        <div v-for="(faq, index) in faqs" :key="index">
+          <div v-if="index === 0" class="text-lg text-center bg-gray-400 dark:bg-gray-800 text-gray-800 dark:text-gray-300 rounded-lg p-4 font-bold mb-8 mt-8">
+            {{ $t('translators_questions') }}
           </div>
+          <div v-if="index === 4" class="text-lg text-center bg-gray-400 dark:bg-gray-800 text-gray-800 dark:text-gray-300 rounded-lg p-4 font-bold mb-8 mt-8">
+            {{ $t('language_learners_questions') }}
+          </div>
+          <div v-if="index === 6" class="text-lg text-center bg-gray-400 dark:bg-gray-800 text-gray-800 dark:text-gray-300 rounded-lg p-4 font-bold mb-8 mt-8">
+            {{ $t('tests_questions') }}
+          </div>
+          <div v-if="index === 9" class="text-lg text-center bg-gray-400 dark:bg-gray-800 text-gray-800 dark:text-gray-300 rounded-lg p-4 font-bold mb-8 mt-8">
+            {{ $t('translation_features_questions') }}
+          </div>
+        <div class="border border-gray-700 dark:border-gray-200 p-4 rounded-xl mb-4 transition-all duration-300 overflow-hidden"
+        :class="{ 'bg-red-50 border-red-600 shadow-md': activeIndex === index }">
+        <!-- دکمه سوال -->
+        <button
+          @click="toggleAccordion(index)"
+          class="group flex items-center justify-between text-left text-lg font-normal leading-8 text-gray-900 dark:text-gray-100 w-full transition duration-300 hover:text-red-600"
+        >
+          <h5 :class="{ 'font-medium text-red-600': activeIndex === index }">
+            {{ faq.question }}
+          </h5>
+
+          <!-- آیکون تغییر -->
+          <svg
+            class="w-6 h-6 transition-transform duration-300"
+            :class="{ 'rotate-180 text-red-600': activeIndex === index }"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 12H18M12 18V6"
+              stroke="currentColor"
+              stroke-width="1.6"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+        </button>
+
+        <!-- جواب با انیمیشن باز و بسته شدن -->
+        <div
+          class="transition-all duration-500 ease-in-out"
+          :style="{
+            maxHeight: activeIndex === index ? '150px' : '0px',
+            opacity: activeIndex === index ? 1 : 0,
+            transform: activeIndex === index ? 'translateY(0)' : 'translateY(-10px)'
+          }"
+        >
+          <p class="text-base text-gray-900 dark:text-gray-100 font-normal leading-6 p-3">
+            {{ faq.answer }}
+          </p>
+        </div>
+        </div>
+
         </div>
       </div>
     </section>
