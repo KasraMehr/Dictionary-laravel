1707bbc31733b1d9e93551ea13de4f9047164313
KasraMehr <kasramehrali@gmail.com>
2025-03-27 12:30:34 -0400

    fixed code spaces

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -223,27 +223,27 @@ function handleImageError() {
                         </div>
 
 
-                 <!-- بخش یادگیری -->
-                 <div class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-gradient-to-br from-gray-400/50 to-gray-200/50 dark:from-gray-800/50 dark:to-gray-700/50 p-6 shadow-lg ring-1 ring-white/10 transition duration-300 hover:ring-white/20 hover:shadow-xl hover:shadow-[#FF2D20]/10 lg:p-10 lg:pb-10 w-full backdrop-blur-sm">
-                   <h2 class="text-xl font-bold text-[#FF2D20]">{{ $t('learning_section') }}</h2>
-                   <p class="text-sm text-gray-700 dark:text-gray-300">{{ $t('learn_new_words_daily') }}</p>
+                        <!-- بخش یادگیری -->
+                        <div class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-gradient-to-br from-gray-400/50 to-gray-200/50 dark:from-gray-800/50 dark:to-gray-700/50 p-6 shadow-lg ring-1 ring-white/10 transition duration-300 hover:ring-white/20 hover:shadow-xl hover:shadow-[#FF2D20]/10 lg:p-10 lg:pb-10 w-full backdrop-blur-sm">
+                          <h2 class="text-xl font-bold text-[#FF2D20]">{{ $t('learning_section') }}</h2>
+                          <p class="text-sm text-gray-700 dark:text-gray-300">{{ $t('learn_new_words_daily') }}</p>
 
-                   <ul class="w-full space-y-2">
-                     <p class="font-semibold text-lg my-4">{{ $t('daily_words') }} </p>
-                     <li v-for="(word, index) in wordList" :key="index"
-                         class="p-1 bg-gray-700/50 rounded-lg text-white flex justify-between
-                                hover:bg-gray-600 transition-colors duration-200">
+                          <ul class="w-full space-y-2">
+                            <p class="font-semibold text-lg my-4">{{ $t('daily_words') }} </p>
+                            <li v-for="(word, index) in wordList" :key="index"
+                                class="p-1 bg-gray-700/50 rounded-lg text-white flex justify-between
+                                       hover:bg-gray-600 transition-colors duration-200">
 
-                       <a :href="`/word/${word.native_lang}-${word.translated_lang}/${word.id}`"
-                          class="flex justify-between items-center w-full h-full p-2">
-                         <span>{{ word.word }}</span>
-                         <button @click.stop="addToFavorites(word)"
-                                 class="text-yellow-400 hover:text-yellow-600">⭐</button>
-                       </a>
+                              <a :href="`/word/${word.native_lang}-${word.translated_lang}/${word.id}`"
+                                 class="flex justify-between items-center w-full h-full p-2">
+                                <span>{{ word.word }}</span>
+                                <button @click.stop="addToFavorites(word)"
+                                        class="text-yellow-400 hover:text-yellow-600">⭐</button>
+                              </a>
 
-                     </li>
-                   </ul>
-                </div>
+                            </li>
+                          </ul>
+                       </div>
                     </div>
                  </main>
              </div>
