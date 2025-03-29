05ad73964ab667a697db05e388ff49d46c865668
KasraMehr <kasramehrali@gmail.com>
2025-03-29 18:43:05 -0400

    fixed links.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -99,17 +99,14 @@ const setLanguage = (lang) => {
                                         </NavLink>
                                     </template>
                                     <NavLink :href="route('library')" :active="route().current('library')" class="text-black dark:text-white">
-                                        {{ $t('library') }}
+                                        {{ $t('dictionary') }}
                                     </NavLink>
-                                    <NavLink :href="route('reports')" :active="route().current('reports')" class="text-black dark:text-white">
-                                        {{ $t('reports') }}
+                                    <NavLink :href="route('learn')" :active="route().current('reports')" class="text-black dark:text-white">
+                                        {{ $t('learn') }}
                                     </NavLink>
-                                    <a href="https://docs.modern-dictionary.com/"
-                                       class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-800 dark:text-white/70 hover:text-black dark:hover:text-white/90 border-b-2 border-transparent rounded-lg hover:bg-gray-300 dark:hover:bg-gray-700/50 hover:ring-black dark:hover:ring-white/20 hover:shadow-xl hover:shadow-[#FF2D20]/10 transition-all duration-300 transform hover:scale-105 focus:outline-none"
-                                       target="_blank"
-                                       rel="noopener noreferrer">
-                                        {{ $t('documentation') }}
-                                    </a>
+                                    <NavLink :href="route('learn')" :active="route().current('reports')" class="text-black dark:text-white">
+                                        {{ $t('quiz') }}
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
