5548051b26e11ff17a3e8077bf7944288e359922
kasramehr <kasramehrali@gmail.com>
2025-04-06 20:04:04 +0330

    added Achievement controller

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Achievement;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AchievementController extends Controller
 {
-    //
+    public function index(): \Inertia\Response
+    {
+        $achievements =  Achievement::all();
+        return Inertia::render('Learn/Achievements', [
+            'achievements' => $achievements,
+        ]);
+    }
 }
