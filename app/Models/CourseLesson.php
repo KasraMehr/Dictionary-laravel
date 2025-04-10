274ef6b0482b0438619c380ec116f3ec02e6e03c
KasraMehr <kasramehrali@gmail.com>
2025-04-09 20:37:53 -0400

    fixed CourseLesson Model data and relations

--- app/Models/CourseLesson.php
+++ app/Models/CourseLesson.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 
 class CourseLesson extends Model
 {
-    //
+  protected $fillable = ['title', 'content', 'video_url', 'order'];
+
+  public function course(): BelongsTo
+  {
+      return $this->belongsTo(Course::class);
+  }
+
+  public function quiz(): HasOne
+  {
+      return $this->HasOne(Quiz::class, 'quiz_id');
+  }
 }
