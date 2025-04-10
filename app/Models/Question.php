be2e455a3cfd8d6465b8e754f9e01d6c11d7ae24
KasraMehr <kasramehrali@gmail.com>
2025-04-09 20:38:27 -0400

    fixed Question Model data and relations

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Question extends Model
 {
-    //
+  protected $fillable = ['question_text', 'question_type', 'options', 'correct_answer', 'points'];
+
+  public function quiz(): BelongsTo
+  {
+    return $this->belongsTo(Quiz::class);
+  }
 }
