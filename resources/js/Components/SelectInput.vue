035b86fab93b86708bc7583a8ef18459eb4256c7
KasraMehr <kasramehrali@gmail.com>
2025-04-10 19:08:27 -0400

    added component Select Input

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <select
+    :id="id"
+    class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs"
+  >
+    <slot></slot>
+  </select>
+</template>
+
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: [String, Number, Boolean],
+    default: ''
+  }
+});
+
+defineEmits(['update:modelValue']);
+</script>
