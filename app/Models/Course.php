824d444c0f67b3e03a1ef7c81e91b005c781477e
KasraMehr <kasramehrali@gmail.com>
2025-04-09 20:37:42 -0400

    fixed Course Model data and relations

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Course extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'trailer_url', 'description', 'level', 'topic',
+     'is_free', 'thumbnail', 'language', 'status'];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'course_user', 'course_id', 'user_id');
+    }
+
+    public function course_lessons(): HasMany
+    {
+        return $this->hasMany(CourseLesson::class);
+    }
+
+    public function quizzes(): HasMany
+    {
+        return $this->hasMany(Quiz::class);
+    }
+
+    public function resources(): HasMany
+    {
+        return $this->hasMany(Resource::class);
+    }
 }
