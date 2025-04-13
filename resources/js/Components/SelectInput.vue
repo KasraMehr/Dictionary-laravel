ade4189a205bbe5a6b1a1d2fa1a7b0f51df9aa0c
kasramehr <kasramehrali@gmail.com>
2025-04-14 00:45:06 +0330

    fixed colors

--- resources/js/Components/SelectInput.vue
+++ resources/js/Components/SelectInput.vue
@@ -1,7 +1,7 @@
 <template>
   <select
     :id="id"
-    class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+    class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 shadow-sm focus:border-red-500 focus:ring-red-600"
     :value="modelValue"
     @change="$emit('update:modelValue', $event.target.value)"
     v-bind="$attrs"
