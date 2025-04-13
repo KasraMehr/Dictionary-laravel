d788bab1d78ee9f6a19a50405e337615cabdb13c
kasramehr <kasramehrali@gmail.com>
2025-04-14 00:46:00 +0330

    fixed colors

--- resources/js/Components/TextArea.vue
+++ resources/js/Components/TextArea.vue
@@ -1,7 +1,7 @@
 <template>
   <textarea
     :id="id"
-    class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+    class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 shadow-sm focus:border-red-500 focus:ring-red-600"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value)"
     v-bind="$attrs"
