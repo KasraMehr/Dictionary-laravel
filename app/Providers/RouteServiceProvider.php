1d6af0d05eb631619e7506f979363cd9a2cf3c9e
kasramehr <kasramehrali@gmail.com>
2025-04-14 17:41:33 +0330

    fixed route service provider

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * مسیر پیش‌فرض بعد از لاگین
+     */
+    public const HOME = '/landing';
+
+    /**
+     * Bootstrap your route-related services.
+     */
+    public function boot(): void
+    {
+        $this->routes(function () {
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+        });
+    }
+
+    public static function homeRoute(): string
+    {
+        $user = auth()->user();
+        return match ($user->role) {
+            'student' => route('student.dashboard'),
+            'teacher' => route('teacher.dashboard'),
+            'translator' => route('translator.dashboard'),
+            default => self::HOME,
+        };
+    }
+}
