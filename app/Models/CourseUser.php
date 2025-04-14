ad8d78550258b8d9b41522f9a36aa20089cc3907
kasramehr <kasramehrali@gmail.com>
2025-04-15 02:55:29 +0330

    added CourseUser.php Model

---
+++ app/Models/CourseUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourseUser extends Model
+{
+    protected $table = 'course_user';
+
+    protected $fillable = [
+        'enrolled_at',
+        'progress',
+        'is_favorite',
+        'completed_at',
+        'rating',
+        'review',
+        'last_accessed_at'
+    ];
+
+    protected $casts = [
+        'completed_at' => 'datetime',
+        'last_accessed_at' => 'datetime',
+        'is_favorite' => 'boolean'
+    ];
+}
