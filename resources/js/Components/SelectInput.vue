0162ff06ecf947c0551d69fa02f5a40519c31d02
KasraMehr <kasramehrali@gmail.com>
2025-04-18 18:16:17 -0400

    fixed UI

--- resources/js/Components/SelectInput.vue
+++ resources/js/Components/SelectInput.vue
@@ -1,7 +1,7 @@
 <template>
   <select
     :id="id"
-    class="mt-1 block w-full rounded-md border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 shadow-sm focus:border-red-500 focus:ring-red-600"
+    class="mt-1 block w-full rounded-md border border-gray-300 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-red-500 focus:border-red-500 transition-all"
     :value="modelValue"
     @change="$emit('update:modelValue', $event.target.value)"
     v-bind="$attrs"
