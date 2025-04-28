b049a6f4e9d4e1cbd2805cc2cdae80bb1e33b71e
kasramehr <kasramehrali@gmail.com>
2025-04-29 01:56:30 +0330

    added level lesson functions levelLesson and fetch words to LearnController.php

--- app/Http/Controllers/General/LearnController.php
+++ app/Http/Controllers/General/LearnController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\General;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Word;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 
@@ -12,6 +14,46 @@ class LearnController extends Controller
     public function learn()
     {
       return Inertia::render('General/Learn/Learn');
+    }
+
+    public function levelLesson($level)
+    {
+        $words = Word::with(['categories:id,name'])
+            ->where('level', $level)
+            ->paginate(10);
+
+        return Inertia::render('General/Learn/LearnLevel', [
+            'level' => $level,
+            'words' => $words->items(),
+            'pagination' => [
+                'current_page' => $words->currentPage(),
+                'last_page' => $words->lastPage(),
+                'per_page' => $words->perPage(),
+                'total' => $words->total(),
+            ]
+        ]);
+    }
+
+    public function fetchWords(Request $request)
+    {
+        $request->validate([
+            'level' => 'required|string|in:A1,A2,B1,B2,C1,C2',
+            'page' => 'required|integer|min:1'
+        ]);
+
+        $words = Word::with(['categories:id,name'])
+            ->where('level', $request->level)
+            ->paginate(10, ['*'], 'page', $request->page);
+
+        return response()->json([
+            'words' => $words->items(),
+            'pagination' => [
+                'current_page' => $words->currentPage(),
+                'last_page' => $words->lastPage(),
+                'per_page' => $words->perPage(),
+                'total' => $words->total(),
+            ]
+        ]);
     }
 
     public function topics()
