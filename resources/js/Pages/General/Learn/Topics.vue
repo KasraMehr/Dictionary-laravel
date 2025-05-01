2b72b6305a67296383116145496ada6f99192354
KasraMehr <kasramehrali@gmail.com>
2025-05-01 16:51:42 -0400

    fixed colors

--- resources/js/Pages/General/Learn/Topics.vue
+++ resources/js/Pages/General/Learn/Topics.vue
@@ -105,7 +105,7 @@ const getCategoryIcon = (slug) => {
                         <div class="flex items-center mb-4">
                             <!-- آیکون -->
                             <div
-                                class="p-3 rounded-lg mr-4"
+                                class="p-3 rounded-lg mx-4"
                                 :class="getCategoryColor(category.id).iconBg"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -130,7 +130,7 @@ const getCategoryIcon = (slug) => {
                             <button class="font-medium flex items-center"
                                     :class="getCategoryColor(category.id).button">
                                 مشاهده موضوع
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mx-1" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                                 </svg>
                             </button>
