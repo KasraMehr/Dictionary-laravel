e876c8e453661de943885a480770dad496bf4a5d
kasramehr <kasramehrali@gmail.com>
2025-05-04 02:25:18 +0330

    added Teacher.php Model

---
+++ app/Models/Teacher.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Teacher extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'bio',
+        'profile_photo_path',
+        'languages',
+        'experiences',
+        'social_links',
+        'teaching_methods',
+        'phone',
+        'website',
+        'location'
+    ];
+
+    protected $casts = [
+        'languages' => 'array',
+        'experiences' => 'array',
+        'social_links' => 'array',
+        'teaching_methods' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function profilePhotoUrl(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if ($this->profile_photo_path) {
+                    return asset('storage/' . $this->profile_photo_path);
+                }
+                return asset('images/default-profile.jpg');
+            }
+        );
+    }
+}
