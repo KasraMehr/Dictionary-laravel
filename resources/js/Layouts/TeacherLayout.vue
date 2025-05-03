d924a944c51d01c545ea0fc710e318a16cfa4252
kasramehr <kasramehrali@gmail.com>
2025-05-04 02:25:41 +0330

    fixed link through teacher profile

--- resources/js/Layouts/TeacherLayout.vue
+++ resources/js/Layouts/TeacherLayout.vue
@@ -79,7 +79,7 @@ const logout = () => {
                           </div>
 
                           <!-- Navigation Links -->
-                          <DropdownLink :href="route('profile.show')" class="flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700">
+                          <DropdownLink :href="route('teacher.profile')" class="flex items-center px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700">
                             <span class="flex items-center gap-1">
                               <svg class="size-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
