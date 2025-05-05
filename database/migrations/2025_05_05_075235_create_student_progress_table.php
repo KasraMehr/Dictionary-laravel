116cdfed2240d3cf93ccccf1fb1e6451416f0cef
kasramehr <kasramehrali@gmail.com>
2025-05-05 12:40:24 +0330

    added migration student progress

---
+++ database/migrations/2025_05_05_075235_create_student_progress_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('language_id')->constrained(); // جدول languages باید ایجاد شود
+            $table->integer('level')->default(1); // سطح فعلی
+            $table->integer('xp')->default(0); // امتیاز تجربه
+            $table->integer('lessons_completed')->default(0);
+            $table->integer('words_learned')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_progress');
+    }
+};
