f4f2a8477852a5dd9b33dc89423680fd860d3c60
kasramehr <kasramehrali@gmail.com>
2025-05-13 00:24:22 +0330

    fixed image bug in Courses.vue

--- resources/js/Pages/General/Learn/Courses.vue
+++ resources/js/Pages/General/Learn/Courses.vue
@@ -12,11 +12,9 @@ const props = defineProps({
   topics: Array
 })
 
-// حالت تاریک/روشن
-const darkMode = ref(false)
-const toggleDarkMode = () => {
-  darkMode.value = !darkMode.value
-}
+const setDefaultImage = (event) => {
+    event.target.src = "/images/default-image.jpg";
+};
 
 // فیلترها
 const searchQuery = ref(props.filters.search || '')
@@ -54,17 +52,6 @@ const resetFilters = () => {
             <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
               دوره‌های آموزشی
             </h1>
-            <button
-              @click="toggleDarkMode"
-              class="p-2 rounded-full bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-200"
-            >
-              <svg v-if="darkMode" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-              </svg>
-            </button>
           </div>
         </header>
 
@@ -174,7 +161,7 @@ const resetFilters = () => {
                     <img
                       :src="course.thumbnail"
                       :alt="course.title"
-                      class="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+                      class="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110" @error="setDefaultImage"
                     >
                     <div class="absolute inset-0 bg-gradient-to-t from-gray-900/70 to-transparent"></div>
                     <div class="absolute bottom-4 right-4">
