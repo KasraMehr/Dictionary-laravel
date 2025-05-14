109cd1e08759814ae4f66a4db8cfdfde21fd6318
kasramehr <kasramehrali@gmail.com>
2025-05-14 10:24:17 +0330

    deleted examples

--- resources/js/Pages/General/Dictionary/Index.vue
+++ resources/js/Pages/General/Dictionary/Index.vue
@@ -448,28 +448,7 @@ onMounted(() => {
                     </div>
 
                     <!-- Word Details -->
-                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        <!-- Examples -->
-                        <div class="bg-gray-100 dark:bg-gray-700/50 rounded-xl p-5">
-                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white mb-3 flex items-center gap-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-[#FF2D20]">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                                </svg>
-                                {{ $t('examples') }}
-                            </h3>
-                            <div class="space-y-2">
-                                <div v-if="selectedWord.example1" class="p-3 bg-white dark:bg-gray-600 rounded-lg border border-gray-200 dark:border-gray-500">
-                                    {{ selectedWord.example1 }}
-                                </div>
-                                <div v-if="selectedWord.example2" class="p-3 bg-white dark:bg-gray-600 rounded-lg border border-gray-200 dark:border-gray-500">
-                                    {{ selectedWord.example2 }}
-                                </div>
-                                <div v-if="selectedWord.example3" class="p-3 bg-white dark:bg-gray-600 rounded-lg border border-gray-200 dark:border-gray-500">
-                                    {{ selectedWord.example3 }}
-                                </div>
-                            </div>
-                        </div>
-
+                    <div class="px-12 py-8">
                         <!-- Description -->
                         <div class="bg-gray-100 dark:bg-gray-700/50 rounded-xl p-5">
                             <h3 class="text-lg font-semibold text-gray-900 dark:text-white mb-3 flex items-center gap-2">
