fa251c94dafad9b256606ec8d1007bbe679f9c8b
kasramehr <kasramehrali@gmail.com>
2025-05-14 09:18:36 +0330

    fixed dropdown button colors

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -111,7 +111,7 @@ const setLanguage = (lang) => {
                                         <template #trigger>
                                             <button
                                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md
-                                            text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300
+                                            text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-900 hover:text-gray-700 dark:hover:text-gray-300
                                             focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700
                                             transition ease-in-out duration-150"
                                             >
@@ -164,7 +164,7 @@ const setLanguage = (lang) => {
                                     <template #trigger>
                                         <button
                                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md
-                                        text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300
+                                        text-gray-500 dark:text-gray-400 bg-gray-200 dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300
                                         focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700
                                         transition ease-in-out duration-150"
                                         >
