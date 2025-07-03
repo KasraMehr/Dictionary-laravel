6d429e375925082be33b7e8bff810ab7d9282ac2
KasraMehr <kasramehrali@gmail.com>
2025-07-03 06:36:11 -0400

    removed comments button

--- resources/js/Pages/General/Learn/CourseShow.vue
+++ resources/js/Pages/General/Learn/CourseShow.vue
@@ -163,13 +163,13 @@ const showVideoModal = ref(false)
                                 >
                                     {{ $t('about_instructor') }}
                                 </button>
-                                <button
+                                <!-- <button
                                     @click="activeTab = 'reviews'"
                                     :class="{'border-indigo-500 text-indigo-600 dark:text-indigo-400 dark:border-indigo-400': activeTab === 'reviews', 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300': activeTab !== 'reviews'}"
                                     class="whitespace-nowrap py-4 px-4 border-b-2 font-medium text-sm"
                                 >
                                     {{ $t('student_reviews') }}
-                                </button>
+                                </button> -->
                             </nav>
                         </div>
 
